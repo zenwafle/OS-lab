9b599712267d08a84a12a380f6cf3063d5c60c11
Connor <99942387+zenwafle@users.noreply.github.com>
2024-12-14 16:39:21 -0700

    Update cscan.js

--- diskAlgo/CSCAN/cscan.js
+++ diskAlgo/CSCAN/cscan.js
@@ -12,44 +12,49 @@ function isValidInputNumbers(requestSequence, head) {
 
 
 function cscan_man(requestSequenceCscan, headCscan, sizeCscan) {
-  requestFinalOrderCscan = [headCscan];
-  tmp = 0;
-  //Descending Order
-  tmpAry = [];
-  for(let i = 0; i < requestSequenceCscan.length; ++i) {
-    tmpAry.push(requestSequenceCscan[i]);
-  }
-  requestSequenceCscanSorted = tmpAry.sort(function (a, b) {
-    return b - a;
+  let requestFinalOrderCscan = [headCscan];
+  let totalSeekCountCscan = 0;
+
+  // Sort the request sequence in ascending order
+  let requestSequenceCscanSorted = requestSequenceCscan.slice().sort(function (a, b) {
+    return a - b;
   });
 
-  for (i = 0; i < requestSequenceCscanSorted.length; ++i) {
-    if (headCscan > requestSequenceCscanSorted[i]) {
+  // Find the split point where requests are greater than the head
+  let tmp = 0;
+  for (let i = 0; i < requestSequenceCscanSorted.length; ++i) {
+    if (requestSequenceCscanSorted[i] > headCscan) {
       tmp = i;
       break;
     }
   }
-  for (i = tmp - 1; i >= 0; --i) {
+
+  // Add requests to the right of the head
+  for (let i = tmp; i < requestSequenceCscanSorted.length; ++i) {
     requestFinalOrderCscan.push(requestSequenceCscanSorted[i]);
   }
+
+  // Add the boundary (disk size)
   if (requestFinalOrderCscan[requestFinalOrderCscan.length - 1] !== sizeCscan) {
     requestFinalOrderCscan.push(sizeCscan);
   }
-  for (i = requestSequenceCscanSorted.length - 1; i >= tmp; --i) {
-    if (
-        i === requestSequenceCscanSorted.length - 1 &&
-        requestSequenceCscanSorted[i] !== 0
-    ) {
-      requestFinalOrderCscan.push(0);
-    }
+
+  // Add requests to the left of the head
+  requestFinalOrderCscan.push(0); // Jump to the start
+  for (let i = 0; i < tmp; ++i) {
     requestFinalOrderCscan.push(requestSequenceCscanSorted[i]);
   }
-  totalSeekCountCscan = Math.abs(sizeCscan - headCscan) +
-                      sizeCscan +
-                      requestFinalOrderCscan[requestFinalOrderCscan.length - 1];
+
+  // Calculate total seek count
+  for (let i = 1; i < requestFinalOrderCscan.length; ++i) {
+    totalSeekCountCscan += Math.abs(
+      requestFinalOrderCscan[i] - requestFinalOrderCscan[i - 1]
+    );
+  }
 
   return [totalSeekCountCscan, requestFinalOrderCscan];
 }
+
 
 
 function resetCscanResult() {
